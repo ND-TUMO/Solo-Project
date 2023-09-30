d555154cc501b40b38efbf94270d4eca7e3d7271
ND-TUMO <136917826+ND-TUMO@users.noreply.github.com>
2023-09-30 15:51:18 +0200

    test.py

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 from random import randint 
-lst1 = ['abcdefghijklmnopqrstuvwxyz']
-lst2 = ['1234567890']
+lst1 = ['abcdefghijklmnopqrstuvwxyz1234567890']
 nmb = input('enter a number:')
 assert isinstance(nmb,int),'Your variable nmb is not of type==int()'
 
+total = ''
 for i in range(1,nmb):
-  
+  total=total+lst1[randint(len(lst1))]
+  print(total)
