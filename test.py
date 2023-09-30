47f309ced37f9de8e567f4bb141f91ae32262f59
ND-TUMO <136917826+ND-TUMO@users.noreply.github.com>
2023-09-30 15:48:34 +0200

    test.py

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
+from random import randint 
+lst1 = ['abcdefghijklmnopqrstuvwxyz']
+lst2 = ['1234567890']
+nmb = input('enter a number:')
+assert isinstance(nmb,int),'Your variable nmb is not of type==int()'
 
+for i in range(1,nmb):
+  
